538b53e9610d52f228c3aaebff333ac99547640e
hanyang <2182758293@qq.com>
2018-04-08 18:10:16 +0800

    去掉打卡时间段功能及添加获取七牛 token 逻辑

--- src/pages/post/index.vue
+++ src/pages/post/index.vue
@@ -18,30 +18,6 @@
             <textarea v-model="detail" auto-height maxlength="300" :show-confirm-bar="false" placeholder="请输入详细描述（选填）"></textarea>
         </div>
 
-        <div class="post-box">
-            <h2>设置开始结束日期</h2>
-            <label @click="selectRadio(1)">
-                <i class="radio-btn" :class="{selected: dateType === 1}"></i>
-                长期打卡
-            </label>
-            <label @click="selectRadio(2)">
-                <i class="radio-btn" :class="{selected: dateType === 2}"></i>
-                设置打卡时间段
-            </label>
-            <div class="post-date-box" v-if="isPostDateBoxShow">
-                <div class="post-date-item">
-                    <picker mode="date" :value="startDate" @change="startDateChange">
-                        <p :class="{actived: startDate}" v-text="startDate || '开始日期'"></p>
-                    </picker>
-                </div>
-                <div class="post-date-item">
-                    <picker mode="date" :start="startDate" :value="endDate" @change="endDateChange">
-                        <p :class="{actived: endDate}" v-text="endDate || '结束日期'"></p>
-                    </picker>
-                </div>
-            </div>
-        </div>
-
         <div class="btn generation-plan" :class="{disabled: ! isDisabled}" @click="generationPlan" v-if="! generating">生成计划</div>
         <div class="btn generation-plan disabled" v-else>生成中...</div>
     </div>
@@ -58,40 +34,59 @@
                 detail: '',
                 postImg: '',
 
-                dateType: -1,
-                startDate: '',
-                endDate: '',
-
                 generating: false
             }
         },
 
         computed: {
-            isPostDateBoxShow() {
-                return this.dateType === 2
-            },
             isSelectImg() {
                 return ! this.postImg.startsWith('data')
             },
             isDisabled() {
-                return this.title && this.postImg && this.dateType !== -1
+                return this.title && this.postImg
             }
+        },
+
+        async onLoad() {
+            const app = getApp()
+
+            if (wx.getStorageSync('qiniu')) {
+                app.token = wx.getStorageSync('qiniu').data.token
+                app.domain = wx.getStorageSync('qiniu').data.domain
+
+                return
+            }
+
+            const data = await api.getQiNiuToken()
+
+            if (data.flag !== 1) {
+                wx.showModal({
+                    title: '提示',
+                    content: data.msg,
+                    showCancel: false
+                })
+
+                return
+            }
+
+            const {token, domain} = data.data
+
+            wx.setStorage({
+                key: 'qiniu',
+                data
+            })
+
+            app.token = token
+            app.domain = domain
         },
 
         onUnload() {
             this.title = ''
             this.postImg = ''
             this.detail = ''
-
-            this.dateType = -1
-            this.startDate = ''
-            this.endDate = ''
         },
 
         methods: {
-            selectRadio(value) {
-                this.dateType = value
-            },
             chooseImage() {
                 const app = getApp()
 
@@ -105,31 +100,50 @@
                                 token: app.token
                             },
                             success: (res) => {
-                                this.postImg = `${app.domain}${JSON.parse(res.data).key}`
+                                const data = JSON.parse(res.data)
+
+                                if (res.statusCode !== 200) {
+                                    wx.showModal({
+                                        title: '错误',
+                                        content: data.error,
+                                        showCancel: false
+                                    })
+
+                                    return
+                                }
+                                this.postImg = `${app.domain}${data.key}`
                             }
                         })
                     }
                 })
             },
-            startDateChange(e) {
-                this.startDate = e.mp.detail.value
-            },
-            endDateChange(e) {
-                this.endDate = e.mp.detail.value
-            },
             async generationPlan() {
+                if (! this.title) {
+                    wx.showToast({
+                        title: '请填写标题',
+                        icon: 'none',
+                        duration: 2000
+                    })
+
+                    return
+                }
+
+                if (! this.postImg) {
+                    wx.showToast({
+                        title: '请选择封面图片',
+                        icon: 'none',
+                        duration: 2000
+                    })
+
+                    return
+                }
+
                 this.generating = true
 
                 const params = {
                     planName: this.title,
                     description: this.detail,
-                    cover: this.postImg,
-                    timeType: this.dateType
-                }
-
-                if (this.dateType === 2) {
-                    params.startTime = this.startDate
-                    params.stopTime = this.endDate
+                    cover: this.postImg
                 }
 
                 const data = await api.createDaKa(params)
