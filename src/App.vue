c802b508ef610d93f3bc4aae3106558ca77337e8
hanyang <2182758293@qq.com>
2018-04-08 14:42:29 +0800

    添加获取七牛 token

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <script>
+    import api from '@/api'
+    import {login} from '@/utils'
+
     export default {
-        index: -1,
-        dakaList: []
+        data() {
+            return {
+                index: -1,
+                token: '',
+                dakaList: []
+            }
+        },
+
+        async onLaunch() {
+            await login()
+
+            const data = await api.getQiNiuToken()
+
+            if (data.flag !== 1) {
+                wx.showModal({
+                    title: '提示',
+                    content: data.msg,
+                    showCancel: false
+                })
+
+                return
+            }
+
+            getApp().token = data.data.token
+        }
     }
 </script>
