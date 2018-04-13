0800a05abf3246e0e57ad48baf4a21160d90774d
hanyang <2182758293@qq.com>
2018-04-13 17:31:43 +0800

    add 页面

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ export default {
   // 这个字段走 app.json
     config: {
         // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-        pages: ['^pages/index/index', 'pages/discover/index', 'pages/post/index', 'pages/detail/index', 'pages/invite/index'],
+        pages: ['^pages/index/index', 'pages/discover/index', 'pages/post/index', 'pages/detail/index', 'pages/invite/index', 'pages/add-content/index'],
         window: {
           onReachBottomDistance: 200,
           backgroundTextStyle: 'light',
